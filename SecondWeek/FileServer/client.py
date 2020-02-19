79bc6fb9380f221da77245f2b02856355013d050
Alejandro Castrillon <jairocastrillon@misena.edu.co>
2020-02-19 13:48:24 -0500

    Ajuste: Descarga de archivo e implementacion de sha256 para verificaciòn de archivos

--- SecondWeek/FileServer/client.py
+++ SecondWeek/FileServer/client.py
@@ -1,47 +1,70 @@
-#
-#   Hello World client in Python
-#   Connects REQ socket to tcp://localhost:5555
-#   Sends "Hello" to server, expects "World" back
-#
 
 import zmq
 import sys
 import os.path
-accion = None
-fileName = None
+import hashlib
 
 context = zmq.Context()
 
-#  Socket to talk to server
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
+#Subida de archivo al servidor
 def uploadFile(fileName):
     if os.path.exists(fileName):
         print('ok')
         file = open(fileName, 'rb')
         dataFile = file.read()
-        dataFile = dataFile
-        socket.send_multipart(('upload'.encode("utf-8"), fileName.encode("utf-8"), dataFile))
+        sha256 = hashlib.sha256(dataFile).hexdigest()
+        print(sha256)
+        socket.send_multipart(('upload'.encode("utf-8"), fileName.encode("utf-8"), dataFile, sha256.encode('utf-8')))
         message = socket.recv()
-        print("Received reply %s" % (message))
+        if message.decode('utf-8') == 'ok':
+            print('Subida del archivo %s exitosa' % fileName)
+        else:
+            print("No se pudo subir el archivo %s" % fileName)
     else:
         sys.stderr.write("El archivo %s no existe \n" % fileName)
         raise SystemExit(1)
 
+#Peticion de listar archivos del servidor
 def listFolder():
     socket.send_multipart(('list'.encode("utf-8"), b''))
     message = socket.recv()
-    print(message)
+    print(message.decode('utf-8'))
+
+#Descarga de archivo del servidor al cliente
+def downloadFile(fileName):
+    socket.send_multipart(('download'.encode('utf-8'), fileName.encode('utf-8')))
+    response = socket.recv_multipart()
+    title = response[0].decode('utf-8')
+    content = response[1]
+    print(content)
+    if title != b'' and content != b'':
+        f = open(title, 'wb')
+        f.write(content)
+        f.close()
+        f = open(title, 'rb')
+        sha256 = hashlib.sha256(f.read()).hexdigest()
+        if sha256 == response[2].decode('utf-8'):
+            print("Descarga del archivo %s exitosa" % title)
+        else:
+            print("El archivo %s no existe en el servidor" % fileName)
+    else:
+        print("El archivo %s no existe en el servidor" % fileName)
 
 if len(sys.argv) < 2:
     sys.stderr.write("Se debe usar: python client.py [accion] [nombre_archivo]")
     raise SystemExit(1)
+
 accion = sys.argv[1]
 fileName = None
+
 if len(sys.argv) == 3:
     fileName = sys.argv[2]
 if accion == 'upload':
     uploadFile(fileName)
 elif accion == 'list':
     listFolder()
+elif accion == 'download':
+    downloadFile(fileName)
--- SecondWeek/FileServer/server.py
+++ SecondWeek/FileServer/server.py
@@ -1,43 +1,57 @@
-#
-#   Hello World server in Python
-#   Binds REP socket to tcp://*:5555
-#   Expects b"Hello" from client, replies with b"World"
-#
 
 import time
 import zmq
 import os
+import hashlib
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
-
-def receiveFile(title, content):
+#Recibe el archivo y lo guarda en el servidor
+def receiveFile(title, content, sha256file):
     route = "uploadedFiles/%s" % str(title)
     f = open(route, 'wb')
     f.write(content)
     f.close()
+    f = open(route, 'rb')
+    sha256 = hashlib.sha256(f.read()).hexdigest()
+    f.close()
+    if sha256file == sha256:
+        socket.send_string('ok')
+    else:
+        socket.send_string('error')
 
+#Retorna la lista de archivos que existe en el servidor
 def listFolder():
-    objects = []
+    items = ''
     for (dirpath, dirnames, filenames) in os.walk("uploadedFiles"):
         for file in filenames:
-            socket.send_string(file)
+            items = items + file + '\n'
+    socket.send_string(items)
+
+#Envia archivo desde el servidor al cliente
+def sendFile(fileName):
+    route = "uploadedFiles/%s" % str(fileName)
+    if os.path.exists(route):
+        f = open(route, 'rb')
+        print('Existe')
+        dataFile = f.read()
+        sha256 = hashlib.sha256(dataFile).hexdigest()
+        socket.send_multipart((fileName.encode('utf-8'), dataFile, sha256.encode('utf-8')))
+    else:
+        socket.send_multipart((''.encode('utf-8'), ''.encode('utf-8')))
 
 while True:
     if not os.path.exists("uploadedFiles"):
         os.mkdir("uploadedFiles")
-    #  Wait for next request from client
     message = socket.recv_multipart()
     accion = message[0]
     if accion == b'upload':
-        receiveFile(message[1], message[2])
+        receiveFile(message[1].decode('utf-8'), message[2], message[3].decode('utf-8'))
     elif accion == b'list':
         listFolder()
-    
-    print("Received request: %s" % message)
-
-    #  Do some 'work'
+    elif accion == b'download':
+        sendFile(message[1].decode('utf-8'))
+    print("Peticion recibida: %s" % message)
     time.sleep(1)
-
