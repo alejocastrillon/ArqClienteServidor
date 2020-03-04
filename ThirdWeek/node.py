3a46114dbfeb5fe8ee206d6980d355f300739ad0
Alejandro Castrillon <jairocastrillon@misena.edu.co>
2020-03-04 15:50:05 -0500

    Nodo

--- ThirdWeek/node.py
+++ ThirdWeek/node.py
@@ -10,15 +10,17 @@ import getmac
 
 class Node:
 
-    def __init__(self, host, ident, next):
+    def __init__(self, host, ident, next, prev):
         self.host = host
         self.ident = ident
         self.next = next
+        self.prev = prev
 
 serverSocket = zmq.Context().socket(zmq.REP)
 nextServer = zmq.Context().socket(zmq.REQ)
 node = None
 
+#Get ip 
 def getIp(): 
     try: 
         hostIp = socket.gethostbyname(socket.getfqdn()) 
@@ -40,34 +42,67 @@ def integerHash(hash):
     return int(hash, 16)
 
 def initializeNode(port):
-    print(port)
+    serverSocket.bind("tcp://*:{}".format(port))
     ident = integerHash(hashString(getDistributedString()))
     host = "tcp://{ip}:{port}".format(ip = getIp(), port = port)
-    node = Node(host, ident, None)
-    print("Inicializado nodo con id: %s" % node.ident)
-    serverSocket.bind("tcp://*:{}".format(port))
+    print("Inicializando nodo con id: %s" % ident)
+    return Node(host, ident, Node(host, ident, None), Node(host, ident, None))
+
 
 def receiveRequestConnection(connectingHost, hostId):
-    print(connectingHost)
     hostId = int(hostId, 10)
-    print(hostId)
+    nextHost = None
+    nextId = None
+    prevHost = None
+    prevId = None
+    if node.ident < hostId:
+        if node.next.ident == node.ident:
+            node.next = Node(connectingHost, hostId, None)
+            prevHost = node.host.encode('utf-8')
+            prevId = str(node.ident).encode('utf-8')
+            nextHost = connectingHost.encode('utf-8')
+            nextId = str(hostId).encode('utf-8')
+        else:
+            if node.next.ident > hostId:
+                otherNode = node.next
+                node.next = Node(connectingHost, hostId, otherNode)
+                nextHost = otherNode.host.encode('utf-8')
+                nextId = str(otherNode.ident).encode('utf-8')
+                prevHost = node.host.encode('utf-8')
+                prevId = str(node.ident).encode('utf-8')
+                #serverSocket.send_multipart(('next'.encode('utf-8'), otherNode.host, otherNode.ident))
+            else:
+                nextServer.connect("tcp://{}".format(node.next.host))
+                nextServer.send_multipart(('connect'.encode('utf'), connectingHost.encode('utf-8'), hostId.encode('utf-8')))
+                response = nextServer.recv_multipart()
+                nextHost = response[0]
+                nextId = response[1]
+                prevHost = response[2]
+                prevId = response[3]
+    else:
+        pass
+    serverSocket.send_multipart((nextHost, nextId, prevHost, prevId))
 
 def sendRequestConnection(hostConnect, port):
-    print(hostConnect)
+    serverSocket.bind("tcp://*:{}".format(port))
     socketConnection = zmq.Context().socket(zmq.REQ)
     socketConnection.connect("tcp://{}".format(hostConnect))
     host = "{h}:{p}".format(h = getIp(), p = port)
-    socketConnection.send_multipart(('connect'.encode('utf-8'), host.encode('utf-8'), str(integerHash(hashString(getDistributedString()))).encode('utf-8')))
-    iden = socketConnection.recv_string()
+    ident = integerHash(hashString(getDistributedString()))
+    socketConnection.send_multipart(('connect'.encode('utf-8'), host.encode('utf-8'), str(ident).encode('utf-8')))
+    message = socketConnection.recv_multipart()
+    return Node(host, ident, Node(message[0].decode('utf-8'), message[1].decode('utf-8')))
 
 if not os.path.exists("uploadedFiles"):
     os.mkdir("uploadedFiles")    
 
 if len(sys.argv) == 2:
-    initializeNode(sys.argv[1])
+    node = initializeNode(sys.argv[1])
 elif len(sys.argv) == 4:
     if sys.argv[2] == 'connect':
-        sendRequestConnection(sys.argv[3], sys.argv[1])
+        node = sendRequestConnection(sys.argv[3], sys.argv[1])
+
+print(node)
 
 while True:
     message = serverSocket.recv_multipart()
