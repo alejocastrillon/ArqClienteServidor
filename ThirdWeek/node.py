14241e37034efac7f86345c71980469470013482
alejandro.castrillon <alejandro.castrillon@utp.edu.co>
2020-03-09 13:29:50 -0500

    Ajuste: Conexion de nodos

--- ThirdWeek/node.py
+++ ThirdWeek/node.py
@@ -7,18 +7,19 @@ import random
 import string
 import socket
 import getmac
+import copy
 
 class Node:
 
-    def __init__(self, host, ident, next, prev):
+    def __init__(self, host, ident, next):
         self.host = host
         self.ident = ident
         self.next = next
-        self.prev = prev
 
 serverSocket = zmq.Context().socket(zmq.REP)
-nextServer = zmq.Context().socket(zmq.REQ)
+socketConnection = zmq.Context().socket(zmq.REQ)
 node = None
+rango = []
 
 #Get ip 
 def getIp(): 
@@ -43,55 +44,68 @@ def integerHash(hash):
 
 def initializeNode(port):
     serverSocket.bind("tcp://*:{}".format(port))
+    rango = [0, pow(2, 256)]
     ident = integerHash(hashString(getDistributedString()))
     host = "tcp://{ip}:{port}".format(ip = getIp(), port = port)
     print("Inicializando nodo con id: %s" % ident)
-    return Node(host, ident, Node(host, ident, None), Node(host, ident, None))
+    return Node(host, ident, Node(host, ident, None))
 
 
-def receiveRequestConnection(connectingHost, hostId):
-    hostId = int(hostId, 10)
-    nextHost = None
-    nextId = None
-    prevHost = None
-    prevId = None
-    if node.ident < hostId:
-        if node.next.ident == node.ident:
-            node.next = Node(connectingHost, hostId, None)
-            prevHost = node.host.encode('utf-8')
-            prevId = str(node.ident).encode('utf-8')
-            nextHost = connectingHost.encode('utf-8')
-            nextId = str(hostId).encode('utf-8')
+def receiveRequestConnection(connectingHost, idHost):
+    idHost = int(idHost, 10)
+    if idHost > node.ident:
+        if node.next.ident <= node.ident:
+            nextNode = copy.deepcopy(node.next)
+            node.next.ident = idHost
+            node.next.host = connectingHost
+            serverSocket.send_multipart(('next'.encode('utf-8'), nextNode.host.encode('utf-8'), str(nextNode.ident).encode('utf-8')))
         else:
-            if node.next.ident > hostId:
-                otherNode = node.next
-                node.next = Node(connectingHost, hostId, otherNode)
-                nextHost = otherNode.host.encode('utf-8')
-                nextId = str(otherNode.ident).encode('utf-8')
-                prevHost = node.host.encode('utf-8')
-                prevId = str(node.ident).encode('utf-8')
-                #serverSocket.send_multipart(('next'.encode('utf-8'), otherNode.host, otherNode.ident))
-            else:
-                nextServer.connect("tcp://{}".format(node.next.host))
-                nextServer.send_multipart(('connect'.encode('utf'), connectingHost.encode('utf-8'), hostId.encode('utf-8')))
-                response = nextServer.recv_multipart()
-                nextHost = response[0]
-                nextId = response[1]
-                prevHost = response[2]
-                prevId = response[3]
+            serverSocket.send_multipart(('question'.encode('utf-8'), node.next.host.encode('utf-8')))
     else:
-        pass
-    serverSocket.send_multipart((nextHost, nextId, prevHost, prevId))
+        nextNode = copy.deepcopy(node.next)
+        node.next.ident = idHost
+        node.next.host = connectingHost
+        serverSocket.send_multipart(('next'.encode('utf-8'), nextNode.host.encode('utf-8'), str(nextNode.ident).encode('utf-8')))
+    print(node.next.ident)
+
+def receiveRequestDisconnect(idHost, nextId, nextHost):
+    idHost = int(idHost, 10)
+    nextId = int(nextId, 10)
+    if node.next.ident == idHost:
+        node.next.ident = nextId
+        node.next.host = nextHost
+        print(node.next.ident)
+        serverSocket.send_multipart(('ok'.encode('utf-8'), ''.encode('utf-8')))
+    else:
+        print('disconnect %s' % node.next.host)
+        serverSocket.send_multipart(('question'.encode('utf-8'), node.next.host.encode('utf-8')))
+
+def disconnectNode():
+    print(node.next.host)
+    if node.ident != node.next.ident:
+        socketConnection.connect("tcp://{}".format(node.next.host))
+        socketConnection.send_multipart(('disconnect'.encode('utf-8'), str(node.ident).encode('utf-8'), str(node.next.ident).encode('utf-8'), node.next.host.encode('utf-8')))
+        message = socketConnection.recv_multipart()
+        while message[0].decode('utf-8') == 'question':
+            socketConnection.connect("tcp://{}".format(message[1].decode('utf-8')))
+            socketConnection.send_multipart(('disconnect'.encode('utf-8'), str(node.ident).encode('utf-8'), str(node.next.ident).encode('utf-8'), node.next.host.encode('utf-8')))
+            message = socketConnection.recv_multipart()
 
 def sendRequestConnection(hostConnect, port):
     serverSocket.bind("tcp://*:{}".format(port))
-    socketConnection = zmq.Context().socket(zmq.REQ)
     socketConnection.connect("tcp://{}".format(hostConnect))
     host = "{h}:{p}".format(h = getIp(), p = port)
     ident = integerHash(hashString(getDistributedString()))
+    print(ident)
     socketConnection.send_multipart(('connect'.encode('utf-8'), host.encode('utf-8'), str(ident).encode('utf-8')))
     message = socketConnection.recv_multipart()
-    return Node(host, ident, Node(message[0].decode('utf-8'), message[1].decode('utf-8')))
+    while(message[0].decode('utf-8') == 'question'):
+        socketConnection.connect("tcp://{}".format(message[1].decode('utf-8')))
+        host = "{h}:{p}".format(h = getIp(), p = port)
+        socketConnection.send_multipart(('connect'.encode('utf-8'), host.encode('utf-8'), str(ident).encode('utf-8')))
+        message = socketConnection.recv_multipart()
+    print(message[2].decode('utf-8'))
+    return Node(host, ident, Node(message[1].decode('utf-8'), int(message[2].decode('utf-8'), 10), None))
 
 if not os.path.exists("uploadedFiles"):
     os.mkdir("uploadedFiles")    
@@ -105,10 +119,16 @@ elif len(sys.argv) == 4:
 print(node)
 
 while True:
-    message = serverSocket.recv_multipart()
-    print(message)
-    action = message[0].decode('utf-8')
-    if action == 'connect':
-        receiveRequestConnection(message[1].decode('utf-8'), message[2].decode('utf-8'))
-    else:
-        pass
+    try:
+        message = serverSocket.recv_multipart()
+        print(message)
+        action = message[0].decode('utf-8')
+        if action == 'connect':
+            receiveRequestConnection(message[1].decode('utf-8'), message[2].decode('utf-8'))
+        elif action == 'disconnect':
+            receiveRequestDisconnect(message[1].decode('utf-8'), message[2].decode('utf-8'), message[3].decode('utf-8'))
+        elif action == 'ok':
+            serverSocket.send_multipart(('ok'.encode('utf-8'), ''.encode('utf-8')))
+    except KeyboardInterrupt:
+        disconnectNode()
+        sys.exit()
